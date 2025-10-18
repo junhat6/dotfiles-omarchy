dd2999b3d6e50f72de4287b0317a9f4a66051d12
junichi.hattori <junhat6@gmail.com>
2025-10-18 22:42:39 +0900

    nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- 文字コード設定
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8,sjis,euc-jp,cp932"
